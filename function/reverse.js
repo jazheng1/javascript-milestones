89ffa2f894e61a5aaad4fa1e7d697500bd99ddae
Jason Zheng <jazheng@davidson.edu>
2020-02-17 11:39:00 -0800

    working code

--- function/reverse.js
+++ function/reverse.js
@@ -1,6 +1,6 @@
 function reverseArray(array) {
   let reversedArray = [];
-  // there are other ways to do this, like you can start from the back fo the array and go down
+  // there are other ways to do the loop, like you can start from the back fo the array and go down
   for (let i = 0; i < array.length; i++) {
     console.log(`This is the element that is being pushed into the new array: ${array[array.length - (i + 1)]}`);
     // I did array.length - (i + 1) to have it push the last element
